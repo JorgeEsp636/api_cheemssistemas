3db31db081049f6bb1ee7f7a65f34502625973b7
E2705 <ejahirarevalo@ucundinamarca.edu.co>
2025-06-05 08:26:47 -0500

    configuración CORS para conectar el front

--- api_cheems/api_cheems/settings.py
+++ api_cheems/api_cheems/settings.py
@@ -181,12 +181,35 @@ SIMPLE_JWT = {
 # Configuración de CORS
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
     'http://localhost',
     'http://127.0.0.1',
 ]
 CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
     'http://localhost',
     'http://127.0.0.1',
+]
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
 
 # Configuración de seguridad adicional
